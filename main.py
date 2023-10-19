6d1a67813410c3a044a6e8d20dbc2353f549f270
Andrew Sitalo <andreysitalo09@gmail.com>
2023-10-19 09:55:14 +0300

    fix bug with blacklist

--- main.py
+++ main.py
@@ -19,13 +19,6 @@ is_raffle_start = False
 raffle_members = []
 print(os.getcwd())
 
-def get_blacklist():
-    blacklist = open("/root/predlozhka/blacklist.txt").readlines()
-    for i in range(len(blacklist)):
-        blacklist[i] = blacklist[i].replace("\n", "")
-
-    return blacklist
-
 
 @dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
@@ -36,14 +29,14 @@ async def on_start(message: types.Message):
 
 
 @dp.message_handler(commands=['help'])
-async def on_start(message: types.Message):
+async def on_help(message: types.Message):
     await bot.send_message(chat_id=message.chat.id,
                            text="*** Внимание !!! *** \n\nБот в стадии тестировки и исправления ошибок, так что если вы обнаружили проблему, напишите сюда: \n\n@setta1a",
                            parse_mode="Markdown")
 
 
 @dp.message_handler(commands=['raffle'])
-async def on_start(message: types.Message):
+async def on_raffle(message: types.Message):
     text = message.text.replace("/raffle", "").strip()
     global is_raffle_start
     global raffle_members
@@ -96,64 +89,38 @@ async def on_start(message: types.Message):
 
 
 
-@dp.message_handler(commands=['blacklist'])
-async def on_start(message: types.Message):
-    print(message.chat.id, admin_id)
-    if int(message.chat.id) == int(admin_id):
-        text = message.text.replace("/blacklist", "").strip()
-
-        if not text:
-            blacklist = open("blacklist.txt")
-            mess = ""
-            for user in blacklist.readlines():
-                mess += user
-            await bot.send_message(admin_id, text="Черный писок:  \n" + mess)
-            blacklist.close()
-
-        else:
-            blacklist = open("blacklist.txt", "a")
-            blacklist.write(text + "\n")
-            blacklist.close()
-
-    else:
-        await bot.send_message(message.chat.id, text="Вы не являетесь администратором данного бота.")
-
-
 @dp.message_handler(content_types=["text", "photo", "video"])
 async def check_messages(message: types.Message):
-    if "@" + message["from"].username not in get_blacklist():
-        if message.text:
-            await bot.send_message(admin_id,
-                                   "📨 *** Получено новое сообщение *** \n\n" + message.text,
-                                   parse_mode="Markdown")
-        if message.photo:
-            print(message.photo)
-            if message.caption:
-                await bot.send_photo(admin_id,
-                                     message.photo[-1].file_id,
-                                     caption="📨 *** Получено новое сообщение *** \n\n" + message.caption,
-                                     parse_mode="Markdown")
-            else:
-                await bot.send_photo(admin_id,
-                                     message.photo[-1].file_id,
-                                     caption="📨 *** Получено новое фото *** \n",
-                                     parse_mode="Markdown")
-        if message.video:
-            print(message.video)
-            if message.caption:
-                await bot.send_video(admin_id,
-                                     message.video.file_id,
-                                     caption="📨 *** Получено новое сообщение *** \n\n" + message.caption,
-                                     parse_mode="Markdown")
-            else:
-                await bot.send_video(admin_id,
-                                     message.video.file_id,
-                                     caption="📨 *** Получено новое видео *** \n",
-                                     parse_mode="Markdown")
+    if message.text:
+        await bot.send_message(admin_id,
+                               "📨 *** Получено новое сообщение *** \n\n" + message.text,
+                               parse_mode="Markdown")
+    if message.photo:
+        print(message.photo)
+        if message.caption:
+            await bot.send_photo(admin_id,
+                                 message.photo[-1].file_id,
+                                 caption="📨 *** Получено новое сообщение *** \n\n" + message.caption,
+                                 parse_mode="Markdown")
+        else:
+            await bot.send_photo(admin_id,
+                                 message.photo[-1].file_id,
+                                 caption="📨 *** Получено новое фото *** \n",
+                                 parse_mode="Markdown")
+    if message.video:
+        print(message.video)
+        if message.caption:
+            await bot.send_video(admin_id,
+                                 message.video.file_id,
+                                 caption="📨 *** Получено новое сообщение *** \n\n" + message.caption,
+                                 parse_mode="Markdown")
+        else:
+            await bot.send_video(admin_id,
+                                 message.video.file_id,
+                                 caption="📨 *** Получено новое видео *** \n",
+                                 parse_mode="Markdown")
 
-        await bot.send_message(message.chat.id, text="*** Ваше сообщение отправлено ***", parse_mode="Markdown")
-    else:
-        await bot.send_message(message.chat.id, text="*** Вы в черном списке ***", parse_mode="Markdown")
+    await bot.send_message(message.chat.id, text="*** Ваше сообщение отправлено ***", parse_mode="Markdown")
 
 
 if __name__ == '__main__':
